01db5a2a4719aad7221469d385512cd303502918
Rujun Chen <rujche@microsoft.com>
2020-10-30 07:56:02 +0800

    Init project by https://start.spring.io/

---
+++ src/main/java/chenrujun/springbootoauth2clientsample/SpringBootOauth2ClientSampleApplication.java
@@ -0,0 +1,13 @@
+package chenrujun.springbootoauth2clientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2ClientSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2ClientSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chenrujun/springbootoauth2clientsample/SpringBootOauth2ClientSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package chenrujun.springbootoauth2clientsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2ClientSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
