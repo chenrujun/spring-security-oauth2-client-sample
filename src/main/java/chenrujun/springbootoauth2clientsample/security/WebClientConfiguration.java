ca553889af7199bd7cd78244e11d63b2996d7284
Rujun Chen <rujche@microsoft.com>
2020-11-02 16:20:29 +0800

    1. Remove unnecessary code. 2. Confirm that accessToken and refreshToken can be refreshed if we use WebClient.

--- src/main/java/chenrujun/springbootoauth2clientsample/security/WebClientConfiguration.java
+++ src/main/java/chenrujun/springbootoauth2clientsample/security/WebClientConfiguration.java
@@ -3,9 +3,6 @@ package chenrujun.springbootoauth2clientsample.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
-import org.springframework.security.oauth2.client.RefreshTokenOAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
@@ -15,30 +12,18 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfiguration {
     @Bean
-    WebClient webClient(OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager) {
+    WebClient webClient(
+        ClientRegistrationRepository clientRegistrationRepository,
+        OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
+    ) {
+        OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+            clientRegistrationRepository,
+            oAuth2AuthorizedClientRepository
+        );
         ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
             new ServletOAuth2AuthorizedClientExchangeFilterFunction(oAuth2AuthorizedClientManager);
         return WebClient.builder()
                         .apply(oauth2Client.oauth2Configuration())
                         .build();
-    }
-
-    @Bean
-    public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
-        ClientRegistrationRepository clientRegistrationRepository,
-        OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
-    ) {
-        DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager =
-            new DefaultOAuth2AuthorizedClientManager(
-                clientRegistrationRepository,
-                oAuth2AuthorizedClientRepository
-            );
-        OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider =
-            OAuth2AuthorizedClientProviderBuilder.builder()
-                                                 .authorizationCode()
-                                                 .refreshToken()
-                                                 .build();
-        oAuth2AuthorizedClientManager.setAuthorizedClientProvider(oAuth2AuthorizedClientProvider);
-        return oAuth2AuthorizedClientManager;
     }
 }
